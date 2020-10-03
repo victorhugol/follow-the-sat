9db22e3215fbb14b5189bd04e816e0a3c74b4865
victorhugol <victorsilva698@usp.br>
2020-10-03 13:27:09 -0300

    Ola mundo no container

--- lib/entrada.dart
+++ lib/entrada.dart
@@ -4,7 +4,9 @@ class Entrada extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("data"),
+      child: Center(
+        child: Text("Ola Mundo!"),
+      ),
     );
   }
 }
