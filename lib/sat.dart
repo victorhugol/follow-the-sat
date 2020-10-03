01f2c91f039e4f8676135ebb7e3023084ec54957
victorhugol <victorsilva698@usp.br>
2020-10-03 13:00:44 -0300

    Rotas Sepadas

---
+++ lib/sat.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Sat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("data"),
+    );
+  }
+}
